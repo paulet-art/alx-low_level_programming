e52010888f7ee9b968913f6ea7e7c0c2eb97f889
paulet-art <wanjirupaulet@gmail.com>
2022-09-08 11:16:36 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main -prints text
- *return: 0
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+printf( "%s", "with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
