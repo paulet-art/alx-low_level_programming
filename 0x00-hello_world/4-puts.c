0ee36bf7a3c90c0202d6f6b579afcdcc21b4d8df
paulet-art <wanjirupaulet@gmail.com>
2022-09-08 22:23:52 -0700

    added task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/** main - prints exactly "Programming is like building a multilingual puzzle"
- * return 0
- * 
- * */
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle"
+ * return:0
+ */
 int main(void)
 {
-puts("""Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
