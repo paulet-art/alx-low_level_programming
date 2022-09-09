c584540fa2acce3c9bb0f5af0286ced2aa4b5c2e
paulet-art <wanjirupaulet@gmail.com>
2022-09-09 14:50:26 -0700

    updates

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
  * main-prints all single digit numbers of base 10 starting from 0
- * return:Always (Success)
+ * Return:Always (Success)
  */
-int main (void)
+int main(void)
 {
 	int n;
 
