a08861d7c3e395446313e1cacf5e566190add5e8
paulet-art <wanjirupaulet@gmail.com>
2022-09-09 14:43:19 -0700

    added task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /*
- * main - prints all single digit numbers of base 10 starting from 0
+ * main :entry point
+ * Description : prints all single digit numbers of base 10 starting from 0
  * Return: Always (Success)
  *
  */
