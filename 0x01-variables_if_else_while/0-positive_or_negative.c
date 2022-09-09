6281d728106026c6c40c8da5fc28e4cbbece7231
paulet-art <wanjirupaulet@gmail.com>
2022-09-09 14:03:50 -0700

    updated task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
@@ -21,7 +20,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is negative",n);
+		printf("%d is negative\n",n);
 	}
 	return (0);
 }
