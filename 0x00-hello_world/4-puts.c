9136623b95ec352acfe013372db3d09120c4ad54
paulet-art <wanjirupaulet@gmail.com>
2022-09-08 22:30:06 -0700

    added task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints exactly "Programming is like building a multilingual puzzle"
- * return:0
+ * main - prints text
+ * return value : 0
  */
 int main(void)
 {
