b2063028cdfaffd768f4cb547a7482c7ad66c6a6
paulet-art <wanjirupaulet@gmail.com>
2022-09-09 19:03:30 -0700

    added advanced task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,20 +5,20 @@
  */
 int main(void)
 {
-	int i,k;
+	int i, k;
 
-	for(i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for(k = 0;k < 100;k++)
+		for (k = 0; k < 100; k++)
 		{
-			if(k > i)
+			if (k > i)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
 				putchar((k / 10) + '0');
 				putchar((k % 10) + '0');
-				if(i != 98)
+				if (i != 98)
 				{
 					putchar(',');
 					putchar(' ');
