2593b31132dd3c571e2d740070dd0990fd33c301
paulet-art <wanjirupaulet@gmail.com>
2022-09-09 19:14:35 -0700

    updates

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-	int i,j,k;
+	int i, j, k;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 1; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
-			for(k = 1; k <= 9; k++)
+			for (k = 1; k <= 9; k++)
 			{
-				if(k > j && k > i)
+				if (k > j && k > i)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-						if(i !=7)
+					if (i != 7)
 						{
 							putchar(',');
 							putchar(' ');
@@ -29,5 +29,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
