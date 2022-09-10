c6bf965485aaed8211f8883c3c7e9c5cc086e0c9
paulet-art <wanjirupaulet@gmail.com>
2022-09-09 19:18:12 -0700

    added advanced task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			if (j > i)
 			{
-				putchar("i + '0'");
-				putchar("j + '0'");
-				if (i !=8)
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
 				{
 					putchar(',');
 					putchar(' ');
