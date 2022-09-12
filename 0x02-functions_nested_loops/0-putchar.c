9833ad180866700a2b6f767bf5e15a92eee9cfe6
paulet-art <wanjirupaulet@gmail.com>
2022-09-12 05:34:04 -0700

    added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * main - prints text
+ * Return - Always (Success)
+ */
+int main(void)
+{
+	printf ("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+<h4>Series of files running a specific program <h4/>
+
