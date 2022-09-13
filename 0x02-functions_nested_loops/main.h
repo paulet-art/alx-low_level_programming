5958cb0d70064a4de49ca349f9f70997d7676c9c
paulet-art <wanjirupaulet@gmail.com>
2022-09-13 12:20:40 -0700

    Corrected tasks

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #ifndef
+HOLBERTON_H_INCLUDED
 #define
+HOLBERTON_H_INCLUDED
 
-int _putchar (char)
+int _putchar (char);
 void print_alphabet(void);
 void 
 print_alphabet_x10(void);
@@ -12,7 +14,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 
 #endif
