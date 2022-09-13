d94075ffe367b785a8903f9de0adc91100cff836
paulet-art <wanjirupaulet@gmail.com>
2022-09-13 13:44:32 -0700

    Added task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _islower(int c) - checks for lowercase
+ * Return : 1 if c is lowercase
+ * Return : 0 if otherwise
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a'; c <= 'z)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
