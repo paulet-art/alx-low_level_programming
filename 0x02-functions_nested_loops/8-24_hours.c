34a8f75fbff4c0900a79587eee4b9be9310ec6bb
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 01:16:06 -0700

    Added task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day
  * @h - hour
- * @m - minutes 
+ * @m - minutes
  * Return: Always 0.
  */
 void jack_bauer(void)
