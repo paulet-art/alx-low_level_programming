6ee6169c476f87ea18583fdcda70d103d0e46c93
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 02:01:41 -0700

    Added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add- adds two integers
+ * @a: parameter arguement
+ * @b: parameter arguement
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
