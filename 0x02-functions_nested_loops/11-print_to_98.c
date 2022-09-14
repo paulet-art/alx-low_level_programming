eda9f4e7a9b8d6d19f8b437861dcf8e8cfe14896
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 06:26:57 -0700

    added task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,13 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		while (n <= 98)
-		{
-			_putchar(("%d", n);
-					if (n != 98)
-					{
-					_putchar(", ");
-					}
-					n++;
-					}
-					}
-
-
+			_putchar("%d, ", n--);
+		_putchar("%d\n", n);
+	}
+	else
+	{
+		while (n > 98)
+			_putchar("%d , ", n++);
+		_putchar("%d\n", n);
+	}
+}
