0be59e184d364aedf7b3d53fee9d5e72bea110a5
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 06:08:06 -0700

    Added task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,26 @@
  */
 void times_table(void)
 {
-	int i = 1;
+	int num, mult, prod;
 
-	do {
-		printf("9 * %d = %d\n", i, 9 * i);
-		i++;
-	} while (i <= 10);
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
