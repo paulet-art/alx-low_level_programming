b3a78ecabe7531691a33e01965f1de30043a0946
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 05:16:53 -0700

    Added task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,17 @@
  * print_alphabet_x10 - prints alphabet ten times
  * Return:Always (success)
  */
-void 
+void
 print_alphabet_x10(void)
 {
 	char c, i;
 
 	for (i == 0; i <= 9; i++)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 		_putchar(c);
-		_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
