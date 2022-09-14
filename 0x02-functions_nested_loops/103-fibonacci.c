11b3a8d9e5ab3664f488de92463d155fde5053cf
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 08:39:24 -0700

    Added task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main -  finds and prints the sum of the even-valued terms
+ * main - prints sum of even terms
  * Return: Always 0
  */
 int main(void)
 {
 	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
 
-	while (n3 <= 4,000,000)
+	while (n3 <= 4000000)
 	{
 		n3 = n1 + n2;
 		n1 = n2;
