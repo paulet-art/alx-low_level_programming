9948611388c23baca13143b4dce519a57aee24ac
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 05:53:40 -0700

    Added task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,9 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	_putchar("last_digit = %d",last_digit);
-	return (0);
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
