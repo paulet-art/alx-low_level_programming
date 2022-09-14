d25befbe5a93f289a5bd5228c004134dcc966243
paulet-art <wanjirupaulet@gmail.com>
2022-09-14 07:37:51 -0700

    Added task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints out the fibonacci series
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int t;
+	unsigned long n1 = 0, n2 = 1,n3;
+
+	for (t = 0; t < 50; t++)
+	{
+		n3 = n1 + n2;
+		printf("%lu", n3);
+		n1 = n2;
+		n2 = n3;
+
+		if (t == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
