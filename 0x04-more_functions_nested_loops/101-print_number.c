af1d9cc20b60341f7169bbc12ecd8d9a479bc41f
paulet-art <wanjirupaulet@gmail.com>
2022-09-16 13:47:37 -0700

    Added task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,12 +9,12 @@ void print_number(int n)
 	unsigned int x;
 
 	x = n;
-	if(n < 0)
+	if (n < 0)
 	{
 		_putchar(45);
 		x = -n;
 	}
-	if(x / 10)
+	if (x / 10)
 		print_number(x / 10);
 	_putchar((x % 10) + '0');
 }
