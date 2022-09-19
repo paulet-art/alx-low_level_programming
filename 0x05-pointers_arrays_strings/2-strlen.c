a2c2a4014d2290d03935c97b0f0db99a683df2c3
paulet-art <wanjirupaulet@gmail.com>
2022-09-19 07:39:44 -0700

    Added task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns lenth of a string
+ * @s:parameter string
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		p++;
+	}
+}
