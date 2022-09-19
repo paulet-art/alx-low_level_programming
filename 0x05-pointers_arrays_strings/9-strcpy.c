51705fd60fe5d0dbb56e0d99b91df20807e5e46d
paulet-art <wanjirupaulet@gmail.com>
2022-09-19 13:22:03 -0700

    Added task 12

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string to src
+ * @dest:parameter
+ * @src:parameter
+ * Return: Always 0
+ */
+char *__strcpy(char *dest, char *src);
+{
+	int i = 1;
+
+	do {
+		i++
+			dest[i]=src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
