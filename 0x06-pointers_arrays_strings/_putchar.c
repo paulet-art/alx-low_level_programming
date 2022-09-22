1e3801a77ceff25e2cb9117c28d41d8950cec64d
paulet-art <wanjirupaulet@gmail.com>
2022-09-22 12:22:27 -0700

    Submitted task one

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - prints to stdout
+ * Return:Always 0
+ */
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 
-#endif
+
+#endif /*MAIN_H*/
