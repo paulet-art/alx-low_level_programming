2913c18fb8d73160d7c92629e6ecd76ff7f9f3d9
paulet-art <wanjirupaulet@gmail.com>
2022-09-22 12:46:47 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	dest = "abcdef";
-	src = "ghijkl";
+	int index = 0, dest_len = 0;
 
-	strcat(dest, src);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-	_putchar(dest);
-	_putchar(src);
-
-	return (0);
+	return (dest);
 }
