84111151c3b12d1a8cc00538a721cba9f4c8672a
paulet-art <wanjirupaulet@gmail.com>
2022-09-27 00:26:53 -0700

    Added task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack:parameter
+ * @needle:parameter
+ * Return:Nothing
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	undigned int i = 0, j = 0;
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + 1);
+	}
+	return (0);
+}
