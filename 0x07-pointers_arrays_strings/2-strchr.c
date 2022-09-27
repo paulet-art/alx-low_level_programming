ec99103740dd49429d2ecb2c53eac64a65734783
paulet-art <wanjirupaulet@gmail.com>
2022-09-26 23:57:59 -0700

    Added task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates charactr in string
+ * @s:parameter string
+ * @c:character to locate
+ * Return: Nothing
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; (s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
+}
